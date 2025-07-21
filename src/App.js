452f053074555c7acafe0e9d299d425ac41089f8
lakshay-develop <imlakshyasharma@gmail.com>
2025-07-21 10:51:46 +0530

    Day 1: React project setup complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome on skillsync</h1>
+      <p>Hare Krishna</p>
+    </div>
+  );
+}
+
+export default App;
